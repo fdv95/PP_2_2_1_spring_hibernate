9398bc9018aa1cfa9a593cf2a835be1b16bbc2a8
Andrei Fadeev <fdv95@mail.ru>
2023-01-08 04:17:06 +0300

    Commit_3

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/CarDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/CarDaoImp.java
@@ -9,7 +9,7 @@ import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
-public class CarDapImp implements CarDao {
+public class CarDaoImp implements CarDao {
     @Autowired
     private SessionFactory sessionFactory;
 
